eab3ce3e95ba330d1f1c0469ae6cc55a1f8ec917
fernando leiva <fer_bostero_91@hotmail.com>
2025-05-15 22:44:17 -0300

    correcion de config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://backendcabana.onrender.com' 
-  : 'http://localhost:5000';
+export const API_URL = 'https://backendcabana.onrender.com' 
+ 
